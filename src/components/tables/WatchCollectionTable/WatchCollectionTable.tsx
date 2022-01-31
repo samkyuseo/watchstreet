63b1967ba92f1562665ff5a18e814301e03eaa06
Samuel Lee <samuelkyuseo@gmail.com>
2022-01-30 21:21:04 -0800

    keep that streak

--- src/components/tables/WatchCollectionTable/WatchCollectionTable.tsx
+++ src/components/tables/WatchCollectionTable/WatchCollectionTable.tsx
@@ -1,5 +1,13 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
-import { NONAME } from 'dns';
+import {
+  Box,
+  Flex,
+  Text,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from '@chakra-ui/react';
 import { WatchTableItem, IWatchTableItemProps } from './WatchTableItem';
 
 interface IWatchCollectionTableProps {
@@ -9,11 +17,9 @@ interface IWatchCollectionTableProps {
 const WatchCollectionTable = ({ watches }: IWatchCollectionTableProps) => {
   return (
     <Box
+      // make content always 80% of the vh
       height='80vh'
-      borderRadius='lg'
-      border='1px'
-      borderColor='gray.200'
-      boxShadow='md'
+      // make content scrollable
       overflowX='hidden'
       overflowY='auto'
       // hide scroll bar
@@ -25,6 +31,11 @@ const WatchCollectionTable = ({ watches }: IWatchCollectionTableProps) => {
           background: 'transparent',
         },
       }}
+      // border settings
+      borderRadius='lg'
+      border='1px'
+      borderColor='gray.200'
+      boxShadow='md'
     >
       {/* Header */}
       <Flex borderBottom='1px' borderColor='gray.200' width='100%' p='10px'>
@@ -33,11 +44,49 @@ const WatchCollectionTable = ({ watches }: IWatchCollectionTableProps) => {
         </Text>
       </Flex>
       {/* Table content */}
-      <Box>
-        {watches.map((watch, index) => {
-          return <WatchTableItem key={index} {...watch} />;
-        })}
-      </Box>
+      {watches.map((watch, index) => {
+        return <WatchTableItem key={index} {...watch} />;
+      })}
+      {/* List Header */}
+      <Flex width='100%' p='10px'>
+        <Text variant='bold-text' fontSize='lg'>
+          Lists
+        </Text>
+      </Flex>
+      <Accordion defaultIndex={[0]} allowMultiple fontFamily='barlow'>
+        <AccordionItem>
+          <h2>
+            <AccordionButton _focus={{ outline: 0 }}>
+              <Box flex='1' textAlign='left'>
+                My Holy Grails
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <h2>
+            <AccordionButton _focus={{ outline: 0 }}>
+              <Box flex='1' textAlign='left'>
+                For my Wife
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat .
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </Box>
   );
 };
